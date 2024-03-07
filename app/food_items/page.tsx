f43fca3a3e131ded164cac7aef2146b1fc833b69
SayuriOkuyama <mosyaneko77@gmail.com>
2024-03-07 18:02:51 +0900

    レシピ一覧、タブ切り替えの実装

--- app/food_items/page.tsx
+++ app/food_items/page.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import ArticleCard from '@/components/layouts/ArticleCard'
+import { Button } from '@/components/ui/button'
+import { PiNotePencilLight } from 'react-icons/pi'
+import { PiMagnifyingGlassLight } from 'react-icons/pi'
 
 const page = () => {
-  return <div>フードアイテム一覧</div>
+  return (
+    <main className="pb-24">
+      <PiMagnifyingGlassLight className="ml-auto mr-2 text-lg" />
+      <h3 className="text-center text-lg font-bold mt-8">
+        フードアイテム情報一覧
+      </h3>
+      <div className="flex justify-between mt-4 mb-1">
+        <ul className="flex text-sm items-end ml-2">
+          <li>ベジタリアンの種類</li>/<li>タグ</li>/<li>人気の投稿</li>
+        </ul>
+        <Button className="py-0 pr-2 items-end">
+          <div className="flex items-end">
+            <PiNotePencilLight className="self-end text-lg" />
+            投稿する
+          </div>
+        </Button>
+      </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 pt-1 pb-8 py-4 gap-4 ">
+        <ArticleCard />
+        <ArticleCard />
+        <ArticleCard />
+        <ArticleCard />
+        <ArticleCard />
+        <ArticleCard />
+      </div>
+      <div className="flex pb-8">ページネーション</div>
+    </main>
+  )
 }
 
 export default page
--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -1,30 +1,168 @@
 import ArticleCard from '@/components/layouts/ArticleCard'
 import { Button } from '@/components/ui/button'
 import { PiNotePencilLight } from 'react-icons/pi'
+import { PiMagnifyingGlassLight } from 'react-icons/pi'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { Input } from '@/components/ui/input'
 
 const page = () => {
   return (
     <main className="pb-24">
       <h3 className="text-center text-lg font-bold mt-8">レシピ一覧</h3>
-      <div className="flex justify-between mt-4 mb-1">
-        <ul className="flex text-sm items-end ml-2">
-          <li>ベジタリアンの種類</li>/<li>タグ</li>/<li>人気のレシピ</li>
-        </ul>
-        <Button className="py-0 pr-2 items-end">
-          <div className="flex">
-            <PiNotePencilLight className="self-center" />
+      <div className="flex justify-end mt-4 mb-1">
+        <Button className="py-0 pr-4">
+          <div className="flex items-end">
+            <PiNotePencilLight className="self-end text-lg text-bold" />
             投稿する
           </div>
         </Button>
       </div>
-      <div className="grid grid-cols-2 sm:grid-cols-3 pt-1 pb-8 py-4 gap-4 ">
+      <Tabs defaultValue="vegetarian_type" className="flex flex-col">
+        <TabsList className="w-100 self-center">
+          <TabsTrigger value="vegetarian_type">ベジタリアンの種類</TabsTrigger>
+          <TabsTrigger value="tags">タグ・ワード</TabsTrigger>
+          <TabsTrigger value="popular">人気のレシピ</TabsTrigger>
+        </TabsList>
+        <TabsContent value="vegetarian_type">
+          <Tabs defaultValue="V" className="flex flex-col">
+            <TabsList className="">
+              <TabsTrigger value="V" className="rounded-full bg-color-v">
+                V
+              </TabsTrigger>
+              <TabsTrigger value="Ori" className="rounded-full bg-color-ori">
+                Ori
+              </TabsTrigger>
+              <TabsTrigger value="Ovo" className="rounded-full bg-color-ovo">
+                Ovo
+              </TabsTrigger>
+              <TabsTrigger value="Psc" className="rounded-full bg-color-psc">
+                Psc
+              </TabsTrigger>
+              <TabsTrigger value="Lct" className="rounded-full bg-color-lct">
+                Lct
+              </TabsTrigger>
+              <TabsTrigger value="Pol" className="rounded-full bg-color-pol">
+                Pol
+              </TabsTrigger>
+              <TabsTrigger value="flu" className="rounded-full bg-color-flu">
+                Flu
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="V">
+              <div className="grid grid-cols-2 sm:grid-cols-3 pt-1 pb-8 py-4 gap-4 ">
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+              </div>
+            </TabsContent>
+            <TabsContent value="Ori">
+              <div className="grid grid-cols-2 sm:grid-cols-3 pt-1 pb-8 py-4 gap-4 ">
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+              </div>
+            </TabsContent>
+            <TabsContent value="Ovo">
+              <div className="grid grid-cols-2 sm:grid-cols-3 pt-1 pb-8 py-4 gap-4 ">
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+              </div>
+            </TabsContent>
+            <TabsContent value="Psc">
+              <div className="grid grid-cols-2 sm:grid-cols-3 pt-1 pb-8 py-4 gap-4 ">
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+              </div>
+            </TabsContent>
+            <TabsContent value="Lct">
+              <div className="grid grid-cols-2 sm:grid-cols-3 pt-1 pb-8 py-4 gap-4 ">
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+              </div>
+            </TabsContent>
+            <TabsContent value="Lct">
+              <div className="grid grid-cols-2 sm:grid-cols-3 pt-1 pb-8 py-4 gap-4 ">
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+              </div>
+            </TabsContent>
+            <TabsContent value="Pol">
+              <div className="grid grid-cols-2 sm:grid-cols-3 pt-1 pb-8 py-4 gap-4 ">
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+              </div>
+            </TabsContent>
+            <TabsContent value="flu">
+              <div className="grid grid-cols-2 sm:grid-cols-3 pt-1 pb-8 py-4 gap-4 ">
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+                <ArticleCard />
+              </div>
+            </TabsContent>
+          </Tabs>
+        </TabsContent>
+        <TabsContent value="tags">
+          <div className="flex my-2">
+            <PiMagnifyingGlassLight className="self-center text-lg ml-4 mr-2" />
+            <Input />
+          </div>
+          <div className="grid grid-cols-2 sm:grid-cols-3 pt-1 pb-8 py-4 gap-4 ">
+            <ArticleCard />
+            <ArticleCard />
+            <ArticleCard />
+            <ArticleCard />
+            <ArticleCard />
+            <ArticleCard />
+          </div>
+        </TabsContent>
+        <TabsContent value="popular">
+          <div className="grid grid-cols-2 sm:grid-cols-3 pt-1 pb-8 py-4 gap-4 ">
+            <ArticleCard />
+            <ArticleCard />
+            <ArticleCard />
+            <ArticleCard />
+            <ArticleCard />
+            <ArticleCard />
+          </div>
+        </TabsContent>
+      </Tabs>
+      {/* <div className="grid grid-cols-2 sm:grid-cols-3 pt-1 pb-8 py-4 gap-4 ">
         <ArticleCard />
         <ArticleCard />
         <ArticleCard />
         <ArticleCard />
         <ArticleCard />
         <ArticleCard />
-      </div>
+      </div> */}
       <div className="flex pb-8">ページネーション</div>
     </main>
   )
