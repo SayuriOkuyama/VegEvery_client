c14f694b43300f44656b7d306f26e8609f60b3c8
SayuriOkuyama <mosyaneko77@gmail.com>
2024-03-21 00:29:13 +0900

    レシピ検索機能実装

--- components/layouts/recipes/VegeTypeSort.js
+++ components/layouts/recipes/VegeTypeSort.js
@@ -1,3 +1,4 @@
+import { IconContext } from 'react-icons' //IconContextをインポート
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import React, { useEffect, useRef, useState } from 'react'
 import ArticleCard from '@/components/layouts/ArticleCard.js'
@@ -6,8 +7,9 @@ import { Button } from '@/components/ui/button'
 import { PiMagnifyingGlassLight } from 'react-icons/pi'
 import { Input } from '@/components/ui/input'
 import { useRouter } from 'next/navigation'
+// import { CiSearch } from 'react-icons/ci'
 
-const VegeTypeSort = ({ page, pageData, articles, pageType }) => {
+const VegeTypeSort = ({ type, pageData, articles, path }) => {
   const router = useRouter()
   const refInput = useRef()
 
@@ -22,16 +24,19 @@ const VegeTypeSort = ({ page, pageData, articles, pageType }) => {
     'other_vegetarian',
   ]
 
-  const url = pageType === 'recipes/search/vegan' ? 'recipes' : 'food_items'
+  const handleSearch = () => {
+    const search = refInput.current.value
+    router.push(`/${path}/search?type=${type}&search=${search}&page=1`)
+  }
 
   return (
-    <Tabs defaultValue={page} className="flex flex-col">
+    <Tabs defaultValue={type} className="flex flex-col">
       <TabsList className="">
         <TabsTrigger
           value="vegan"
           className="rounded-full bg-color-v opacity-60 mr-1 text-xs h-8"
           onClick={() => {
-            router.push(`/${url}/search/vegan?page=1`)
+            router.push(`/${path}/search?type=vegan&page=1`)
           }}>
           V
         </TabsTrigger>
@@ -39,7 +44,7 @@ const VegeTypeSort = ({ page, pageData, articles, pageType }) => {
           value="oriental_vegetarian"
           className="rounded-full bg-color-ori opacity-60 mr-1 text-sm w-8 h-8"
           onClick={() => {
-            router.push(`/${url}/search/oriental_vegetarian?page=1`)
+            router.push(`/${path}/search?type=oriental_vegetarian&page=1`)
           }}>
           Ori
         </TabsTrigger>
@@ -47,7 +52,7 @@ const VegeTypeSort = ({ page, pageData, articles, pageType }) => {
           value="ovo_vegetarian"
           className="rounded-full bg-color-ovo opacity-60 mr-1 text-sm w-8 h-8"
           onClick={() => {
-            router.push(`/${url}/search/ovo_vegetarian?page=1`)
+            router.push(`/${path}/search?type=ovo_vegetarian&page=1`)
           }}>
           Ovo
         </TabsTrigger>
@@ -55,7 +60,7 @@ const VegeTypeSort = ({ page, pageData, articles, pageType }) => {
           value="pescatarian"
           className="rounded-full bg-color-psc opacity-60 mr-1 text-sm w-8 h-8"
           onClick={() => {
-            router.push(`/${url}/search/pescatarian?page=1`)
+            router.push(`/${path}/search?type=pescatarian&page=1`)
           }}>
           Psc
         </TabsTrigger>
@@ -63,7 +68,7 @@ const VegeTypeSort = ({ page, pageData, articles, pageType }) => {
           value="lacto_vegetarian"
           className="rounded-full bg-color-lct opacity-60 mr-1 text-sm w-8 h-8"
           onClick={() => {
-            router.push(`/${url}/search/lacto_vegetarian?page=1`)
+            router.push(`/${path}/search?type=lacto_vegetarian&page=1`)
           }}>
           Lct
         </TabsTrigger>
@@ -71,7 +76,7 @@ const VegeTypeSort = ({ page, pageData, articles, pageType }) => {
           value="pollo_vegetarian"
           className="rounded-full bg-color-pol opacity-60 mr-1 text-sm w-8 h-8"
           onClick={() => {
-            router.push(`/${url}/search/pollo_vegetarian?page=1`)
+            router.push(`/${path}/search?type=pollo_vegetarian&page=1`)
           }}>
           Pol
         </TabsTrigger>
@@ -79,7 +84,7 @@ const VegeTypeSort = ({ page, pageData, articles, pageType }) => {
           value="fruitarian"
           className="rounded-full bg-color-flu opacity-60 mr-1 text-sm w-8 h-8"
           onClick={() => {
-            router.push(`/${url}/search/fruitarian?page=1`)
+            router.push(`/${path}/search?type=fruitarian&page=1`)
           }}>
           Flu
         </TabsTrigger>
@@ -87,7 +92,7 @@ const VegeTypeSort = ({ page, pageData, articles, pageType }) => {
           value="other_vegetarian"
           className="rounded-full bg-color-oth opacity-60 text-sm w-8 h-8"
           onClick={() => {
-            router.push(`/${url}/search/other_vegetarian?page=1`)
+            router.push(`/${path}/search?type=other_vegetarian&page=1`)
           }}>
           Oth
         </TabsTrigger>
@@ -95,11 +100,33 @@ const VegeTypeSort = ({ page, pageData, articles, pageType }) => {
       {vegeTags.map(vegeTag => {
         return (
           <TabsContent key={vegeTag} value={vegeTag}>
-            <div className="flex my-2">
-              <Button onClick={() => handleSearch()}>
-                <PiMagnifyingGlassLight className="self-center text-lg ml-4 mr-2" />
+            <div className="container flex items-center justify-end">
+              {/* <Input ref={refInput} /> */}
+              {/* <Button type="submit">
+                  <CiSearch />
+                </Button> */}
+              <Input
+                ref={refInput}
+                type="text"
+                placeholder="search"
+                className="pr-0 block w-64"
+              />
+              {/* <Button type="submit">
+                  <CiSearch />
+                </Button> */}
+              <Button
+                // onClick={e =>
+                //   router.push(
+                //     `/${path}/search?type=${type}&search=${e.target.value}&page=1`,
+                //   )
+                // }
+                onClick={() => handleSearch()}
+                className="p-0 ml-0">
+                <IconContext.Provider
+                  value={{ size: '25px', className: 'p-0 ml-0 mr-0' }}>
+                  <PiMagnifyingGlassLight className="self-center text-lg ml-4 mr-2" />
+                </IconContext.Provider>
               </Button>
-              <Input ref={refInput} />
             </div>
             <div className="grid grid-cols-2 sm:grid-cols-3 pt-1 pb-8 py-4 gap-4 ">
               {articles &&
@@ -129,11 +156,7 @@ const VegeTypeSort = ({ page, pageData, articles, pageType }) => {
                 })}
             </div>
             {pageData && (
-              <PaginationParts
-                pageData={pageData}
-                page={page}
-                pageType={pageType}
-              />
+              <PaginationParts pageData={pageData} type={type} path={path} />
             )}
           </TabsContent>
         )
