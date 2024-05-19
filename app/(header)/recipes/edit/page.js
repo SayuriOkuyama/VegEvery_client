ef2be45743cef16bf36622daff92c0a1971589d4
SayuriOkuyama <mosyaneko77@gmail.com>
2024-05-19 16:17:35 +0900

    Suspense を layout.js に移動

--- app/(header)/recipes/edit/page.js
+++ app/(header)/recipes/edit/page.js
@@ -9,7 +9,7 @@ import EditTags from '@/app/(header)/recipes/edit/EditTags.js'
 import EditMaterials from '@/app/(header)/recipes/edit/EditMaterials.js'
 import EditStep from '@/app/(header)/recipes/edit/EditStep.js'
 import EditFormVegeTypes from '@/app/(header)/recipes/edit/EditFormVegeTypes.js'
-import { useEffect, useState, Suspense } from 'react'
+import { useEffect, useState } from 'react'
 import { PiCameraLight } from 'react-icons/pi'
 import { IconContext } from 'react-icons'
 import { useDropzone } from 'react-dropzone'
@@ -201,132 +201,130 @@ const page = () => {
   if (!data) return <Loading />
 
   return (
-    <Suspense>
-      <main className="pb-32 max-w-2xl">
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 my-16">
-          <EditFormVegeTypes register={register} control={control} />
+    <main className="pb-32 max-w-2xl">
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 my-16">
+        <EditFormVegeTypes register={register} control={control} />
 
-          <div className="bg-orange aspect-[4/3] max-w-2xl mx-auto">
-            {watcher.thumbnail_url ? (
-              <div className="image-preview relative flex w-full h-full">
-                <button
-                  className="absolute right-1 top-1 bg-white w-4 h-4 leading-none"
-                  type="button"
-                  onClick={() => {
-                    setValue('thumbnail_newFile', '')
-                    setValue('thumbnail_url', '')
-                    setValue('thumbnail_path', '')
-                  }}>
-                  ✕
-                </button>
-                <img
-                  src={watcher.thumbnail_url}
-                  className="object-cover w-full h-full block"
-                  alt="Uploaded Image"
-                />
+        <div className="bg-orange aspect-[4/3] max-w-2xl mx-auto">
+          {watcher.thumbnail_url ? (
+            <div className="image-preview relative flex w-full h-full">
+              <button
+                className="absolute right-1 top-1 bg-white w-4 h-4 leading-none"
+                type="button"
+                onClick={() => {
+                  setValue('thumbnail_newFile', '')
+                  setValue('thumbnail_url', '')
+                  setValue('thumbnail_path', '')
+                }}>
+                ✕
+              </button>
+              <img
+                src={watcher.thumbnail_url}
+                className="object-cover w-full h-full block"
+                alt="Uploaded Image"
+              />
+            </div>
+          ) : (
+            <div {...getRootProps()} className="w-full h-full">
+              <input accept="image/*" {...getInputProps()} />
+              <div className="h-full flex justify-center items-center">
+                <IconContext.Provider value={{ color: '#ccc', size: '80px' }}>
+                  <PiCameraLight />
+                </IconContext.Provider>
               </div>
-            ) : (
-              <div {...getRootProps()} className="w-full h-full">
-                <input accept="image/*" {...getInputProps()} />
-                <div className="h-full flex justify-center items-center">
-                  <IconContext.Provider value={{ color: '#ccc', size: '80px' }}>
-                    <PiCameraLight />
-                  </IconContext.Provider>
-                </div>
-              </div>
-            )}
-          </div>
-          {errors.thumbnail_newFile && (
-            <div className="container text-red-400">
-              {errors.thumbnail_newFile.message}
             </div>
           )}
-
-          <div className="container py-4 space-y-4">
-            <div>
-              <h3>レシピタイトル</h3>
-              <input
-                className="border w-full"
-                type="text"
-                {...register(`title`)}
-              />
-              {errors.title && (
-                <div className="text-red-400">{errors.title.message}</div>
-              )}
-            </div>
-            <EditTags register={register} control={control} errors={errors} />
-            <div>
-              <h3>調理目安時間</h3>
-              <input
-                className="border w-8 px-1"
-                type="number"
-                placeholder="20"
-                {...register(`time`)}
-              />
-              分
-              {errors.time && (
-                <div className="text-red-400">{errors.time.message}</div>
-              )}
-            </div>
+        </div>
+        {errors.thumbnail_newFile && (
+          <div className="container text-red-400">
+            {errors.thumbnail_newFile.message}
           </div>
+        )}
 
-          <EditMaterials
-            register={register}
-            control={control}
-            setValue={setValue}
-            errors={errors}
-          />
+        <div className="container py-4 space-y-4">
+          <div>
+            <h3>レシピタイトル</h3>
+            <input
+              className="border w-full"
+              type="text"
+              {...register(`title`)}
+            />
+            {errors.title && (
+              <div className="text-red-400">{errors.title.message}</div>
+            )}
+          </div>
+          <EditTags register={register} control={control} errors={errors} />
+          <div>
+            <h3>調理目安時間</h3>
+            <input
+              className="border w-8 px-1"
+              type="number"
+              placeholder="20"
+              {...register(`time`)}
+            />
+            分
+            {errors.time && (
+              <div className="text-red-400">{errors.time.message}</div>
+            )}
+          </div>
+        </div>
 
-          <EditStep
-            register={register}
-            control={control}
-            stepImages={stepImages}
-            setStepImages={setStepImages}
-            setValue={setValue}
-            reset={reset}
-            errors={errors}
-          />
-          <hr className="mx-4" />
+        <EditMaterials
+          register={register}
+          control={control}
+          setValue={setValue}
+          errors={errors}
+        />
 
-          <div className="flex justify-center">
-            <Dialog className="mx-auto">
-              <DialogTrigger className="mx-auto">
-                <Button
-                  type="button"
-                  className="mx-auto bg-button block py-1 mt-16 border-button-color">
-                  削除
-                </Button>
-              </DialogTrigger>
-              <DialogContent>
-                <DialogHeader>
-                  <DialogTitle>本当に削除しますか？</DialogTitle>
-                  <DialogDescription className="flex">
+        <EditStep
+          register={register}
+          control={control}
+          stepImages={stepImages}
+          setStepImages={setStepImages}
+          setValue={setValue}
+          reset={reset}
+          errors={errors}
+        />
+        <hr className="mx-4" />
+
+        <div className="flex justify-center">
+          <Dialog className="mx-auto">
+            <DialogTrigger className="mx-auto">
+              <Button
+                type="button"
+                className="mx-auto bg-button block py-1 mt-16 border-button-color">
+                削除
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>本当に削除しますか？</DialogTitle>
+                <DialogDescription className="flex">
+                  <Button
+                    onClick={handleDelete}
+                    type="button"
+                    className="mx-auto bg-button block py-1 mt-8 border-button-color">
+                    削除する
+                  </Button>
+                  <DialogClose asChild>
                     <Button
-                      onClick={handleDelete}
                       type="button"
                       className="mx-auto bg-button block py-1 mt-8 border-button-color">
-                      削除する
+                      戻る
                     </Button>
-                    <DialogClose asChild>
-                      <Button
-                        type="button"
-                        className="mx-auto bg-button block py-1 mt-8 border-button-color">
-                        戻る
-                      </Button>
-                    </DialogClose>
-                  </DialogDescription>
-                </DialogHeader>
-              </DialogContent>
-            </Dialog>
-            <Button
-              type="submit"
-              className="mx-auto bg-button block py-1 mt-16 border-button-color">
-              更新
-            </Button>
-          </div>
-        </form>
-      </main>
-    </Suspense>
+                  </DialogClose>
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+          <Button
+            type="submit"
+            className="mx-auto bg-button block py-1 mt-16 border-button-color">
+            更新
+          </Button>
+        </div>
+      </form>
+    </main>
   )
 }
 
